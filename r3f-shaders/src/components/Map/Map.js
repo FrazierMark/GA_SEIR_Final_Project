288e87cef967d6544585029b3ed2e15f91bb03ee
FrazierMark <m.mark.frazier@gmail.com>
2022-05-17 16:05:04 -0700

    Still figuring out mapbox gl

--- r3f-shaders/src/components/Map/Map.js
+++ r3f-shaders/src/components/Map/Map.js
@@ -1,4 +1,4 @@
-import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+import ReactMapboxGl, { Layer, Feature, Source } from 'react-mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 
@@ -7,21 +7,29 @@ const Map = ReactMapboxGl({
         'pk.eyJ1IjoiZnJhemllcm1hcmsiLCJhIjoiY2wzOTBhZzJiMDFwejNqbzJyMGs0YmZ5NCJ9.2VB9C63HoxzjCpCmbhga9A'
 });
 
+const RASTER_SOURCE_OPTIONS = {
+    "type": "vector",
+    "url": "mapbox://mapbox.satellite",
+    "tileSize": 512
+};
+
 const GeoMap = () => {
-    // in render()
     return (
         <>
             <Map
-                style="mapbox://styles/mapbox/streets-v9"
+                style='mapbox://styles/fraziermark/cl3ar5jpn000614kyqab6o8c9'
                 containerStyle={{
                     height: '100vh',
                     width: '100vw'
                 }}
-            >
-                <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
+            />
+            {/* <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
                     <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
-                </Layer>
-            </Map>;
+                </Layer> */}
+            {/* 
+                <Source id="source_id" tileJsonSource={RASTER_SOURCE_OPTIONS} />
+                <Layer type="raster" id="layer_id" sourceId="source_id" /> */}
+
         </>
     )
 }
