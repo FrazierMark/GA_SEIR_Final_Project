54dec41856ee33cfda08d40b71389959aa1058df
Mark Frazier <60164353+FrazierMark@users.noreply.github.com>
2022-05-19 09:16:30 -0700

    Update Map.js
    Change

--- r3f-shaders/src/components/Map/Map.js
+++ r3f-shaders/src/components/Map/Map.js
@@ -8,7 +8,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZnJhemllcm1hcmsiLCJhIjoiY2wzOTBhZzJiMDFwejNqb
 
 const GeoMap = () => {
     const mapContainer = useRef();
-    const [lng, setLng] = useState(5);
+    const [lng, setLng] = useState(4);
     const [lat, setLat] = useState(34);
     const [zoom, setZoom] = useState(1.5);
 
@@ -19,10 +19,18 @@ const GeoMap = () => {
         const map = new mapboxgl.Map({
             container: mapContainer.current,
             style: "mapbox://styles/mapbox/satellite-streets-v11",
-            center: [-111.75, 40.581],
+            center: [lng, lat],
             zoom: 12,
             pitch: 60,
             bearing: 80
+        });
+        // Add navigation control (the +/- zoom buttons)
+        map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+
+        map.on('move', () => {
+            setLng(map.getCenter().lng.toFixed(4));
+            setLat(map.getCenter().lat.toFixed(4));
+            setZoom(map.getZoom().toFixed(2));
         });
 
         // only want to work with the map after it has fully loaded
@@ -41,7 +49,6 @@ const GeoMap = () => {
                 'exaggeration': 1.5
             });
 
-
             // add a sky layer
             // the sky layer is a custom mapbox layer type
             // see https://docs.mapbox.com/mapbox-gl-js/style-spec/layers/#sky
@@ -56,8 +63,8 @@ const GeoMap = () => {
             });
             // Add Fog
             map.setFog({
-                'range': [-0.5, 2],
-                'color': 'red',
+                'range': [-0.5, 20],
+                'color': 'grey',
                 'horizon-blend': 0.1
             });
 
@@ -68,8 +75,15 @@ const GeoMap = () => {
         return () => map.remove();
     }, []);
 
-    return <div ref={mapContainer} style={{ width: "100%", height: "100vh" }} />;
+
+    return (
+        <div>
+            <div className="long_lat_bar">
+                Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+            </div>
+            <div ref={mapContainer} style={{ width: "100%", height: "100vh" }} />
+        </div>
+    )
 };
 
 export default GeoMap
-
