5418ec497b6b3f2abe722ffe4a48a94ce615f6f6
FrazierMark <m.mark.frazier@gmail.com>
2022-05-28 09:34:01 -0700

    Still have this proxy issue...

--- server.js
+++ server.js
@@ -3,15 +3,23 @@ const express = require("express");
 const path = require("path");
 const logger = require("morgan");
 const favicon = require("serve-favicon");
+var bodyParser = require("body-parser");
+var cors = require('cors')
 
 require("./config/database");
 
-// Require controllers here
-
 const app = express();
+app.use(bodyParser.json())
+app.use(cors())
 
 // add in when the app is ready to be deployed
 // app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 app.use(logger("dev"));
 app.use(express.json());
 app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
@@ -30,7 +38,7 @@ app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, function () {
   console.log(`Express app listening on port ${port}`);
--- src/utils/userService.js
+++ src/utils/userService.js
@@ -26,7 +26,6 @@ const options = {
 // }
 
 function signup(user) {
-  console.log(user)
   return axios.post(BASE_URL + "signup", user, options)
     .then((res) => {
       if (res.ok) return res.json();
@@ -50,7 +49,6 @@ function logout() {
 }
 
 function login(creds) {
-  console.log(creds)
   return fetch(BASE_URL + "login", {
     method: "POST",
     headers: new Headers({ "Content-Type": "application/json" }),
@@ -63,22 +61,6 @@ function login(creds) {
     })
     .then(({ token }) => tokenService.setToken(token));
 }
-
-// function login(creds) {
-//   return axios.post(BASE_URL + "login", JSON.stringify(creds), options)
-//     .then((res) => {
-//       if (res.ok) return res.json();
-//       // Probably a duplicate email
-//       throw new Error("Email already taken!");
-//     })
-//     .then((res) => {
-//       // Valid login if we have a status of 2xx (res.ok)
-//       if (res.ok) return res.json();
-//       throw new Error("Bad Credentials!");
-//     })
-//     .then(({ token }) => tokenService.setToken(token));
-// }
-
 
 const userService = {
   signup,
