73461139ab3d52e9d2a02077cae183792a78ad5c
FrazierMark <m.mark.frazier@gmail.com>
2022-06-09 10:35:34 -0700

    Minimizer issue fix?

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+const { default: MiniCssExtractPlugin } = require('mini-css-extract-plugin');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /.s?css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+        ],
+    },
+    optimization: {
+        minimizer: [
+            // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+            // `...`,
+            new CssMinimizerPlugin(),
+        ],
+    },
+    plugins: [new MiniCssExtractPlugin()],
+};
